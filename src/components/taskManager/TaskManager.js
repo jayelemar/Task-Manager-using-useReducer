c946a808cbde84e799bba6e6c855efaea64b8014
Jay Termulo <jetermulo@gmail.com>
2023-08-03 02:48:54 +0800

    build:Added TaskManager JSX mark Up

--- src/components/taskManager/TaskManager.js
+++ src/components/taskManager/TaskManager.js
@@ -3,9 +3,26 @@ import './TaskManager.css'
 
 const TaskManager = () => {
   return (
-    <div>
-        <h1>TaskManager.js</h1>
+    <div className='--bg-primary'>
+        <h1 className='--text-center --text-light'>Task Manager using useState</h1>
+        <div className='--flex-center --p'>
+            <div className='--card --width-500px --p --flex-center'>
+                <form action="" className='--form-control form'>
+                    <div>
+                            <label htmlFor="name">Task</label>
+                            <input type="text" placeholder='Task Name' name='name'/>
+                    </div>
+                    <div>
+                            <label htmlFor="date">Task</label>
+                            <input type="date" name='date'/>
+                    </div>
+                    <button className='--btn --btn-success --btn-block --p'>Save Task</button>
+                </form>
+            </div>
+            
+        </div>
     </div>
+    
   )
 }
 
