c4814ac413e09ec046d071bd7fea21cbb88e48e2
Jay Termulo <jetermulo@gmail.com>
2023-08-04 01:06:10 +0800

    build: Added Task.js JSX file

--- src/components/taskManager/Task.js
+++ src/components/taskManager/Task.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import {FaCheckDouble, FaEdit, FaTrashAlt} from 'react-icons/fa'
+
 
 const Task = () => {
   return (
-    <div>
-    <h1>Task.js</h1>
+    <div className='task' style={{display: 'block'},{width:'500px'}}>
+        <span>
+            <p><b>Task: </b>Task one</p>
+            <p><b>Date: </b>2022-02-02</p>
+        </span>
+        <span>
+            <button><FaEdit color='green'/></button>
+            <button><FaTrashAlt color='red'/></button>
+            <button><FaCheckDouble color='purple'/></button>
+        </span>
     </div>
   )
 }
