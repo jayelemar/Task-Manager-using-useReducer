c2df7d9384e77aa4087f6719e045125b37226223
Jay Termulo <jetermulo@gmail.com>
2023-08-04 10:17:56 +0800

    build: Added Logic for Buttons and Task List

--- src/components/taskManager/Task.js
+++ src/components/taskManager/Task.js
@@ -2,17 +2,19 @@ import React from 'react'
 import {FaCheckDouble, FaEdit, FaTrashAlt} from 'react-icons/fa'
 
 
-const Task = () => {
+const Task = ({id, name, date, isComplete, editTask, deleteTask, completeTask}) => {
   return (
-    <div className='task' style={{display: 'block'},{width:'500px'}}>
+    <div className={isComplete ? "task complete":"task"} 
+            key={id}
+            style={{display: 'block'},{width:'500px'}}>
         <span>
-            <p><b>Task: </b>Task one</p>
-            <p><b>Date: </b>2022-02-02</p>
+            <p><b>Task: </b>{name}</p>
+            <p><b>Date: </b>{date}</p>
         </span>
         <span>
-            <button><FaEdit color='green'/></button>
-            <button><FaTrashAlt color='red'/></button>
-            <button><FaCheckDouble color='purple'/></button>
+            <button onClick={() => editTask(id)}><FaEdit color='green'/></button>
+            <button onClick={() => deleteTask(id)}><FaTrashAlt color='red'/></button>
+            <button onClick={() => completeTask(id)}><FaCheckDouble color='purple'/></button>
         </span>
     </div>
   )
--- src/components/taskManager/TaskManager.js
+++ src/components/taskManager/TaskManager.js
@@ -1,23 +1,116 @@
 import React from 'react'
 import './TaskManager.css'
 import Task from './Task'
+import { useState, useRef, useEffect } from 'react'
+import useLocalStorage from 'use-local-storage'
 
 const TaskManager = () => {
+    const [name, setName] = useState("")
+    const [date, setDate] = useState("")
+    // const [tasks, setTasks] = useState([])
+    const [tasks, setTasks] = useLocalStorage("tasks", [])
+
+    const [taskID, setTaskID] = useState(null)
+    const [isEditing, setIsEditing] = useState(false)
+
+    const nameInputRef = useRef(null);
+    
+
+
+    useEffect(() => {
+        nameInputRef.current.focus()
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!name && !date || !name || !date) {
+            alert("Please enter task name and date")
+        } else if(name && date && isEditing) {
+            setTasks(
+                tasks.map((task) => {
+                    if (task.id === taskID) {
+                        return {...task, name, date, isComplete: false}
+                    } else {
+                        return task;
+                    }
+                })
+            );
+            setName("");
+            setDate("");
+            setIsEditing(false)
+            setTaskID(null)
+        } else {
+            const newTask = {
+                id: Date.now(), 
+                name,
+                date,
+                isComplete: false
+            }
+            setTasks([...tasks, newTask])
+            setName("")
+            setDate("")
+        }
+    };
+
+    const editTask = (id) => {
+        // console.log(id)
+        const thisTask = tasks.find((task) => task.id === id)
+        setIsEditing(true)
+        setTaskID(id)
+        setName(thisTask.name);
+        setDate(thisTask.date);
+    };
+
+    const deleteTask = (id) => {
+        // console.log(id)
+        if(window.confirm("Delete this task") === true) {
+            const newTasks = tasks.filter((task) => task.id !== id);
+            setTasks(newTasks);
+        }
+    };
+
+    const completeTask = (id) => {
+        // console.log(id)
+        setTasks(
+            tasks.map((task) => {
+                if (task.id === id) {
+                    return {...task, isComplete: true}
+                } else {
+                    return task
+                }
+            })
+        )
+    };
+
   return (
     <div className='--bg-primary'>
         <h1 className='--text-center --text-light'>Task Manager using useState</h1>
         <div className='--flex-center --p'>
             <div className='--card --width-500px --p --flex-center'>
-                <form action="" className='--form-control form'>
-                    <div>
+                <form onSubmit={handleSubmit} className='--form-control form'>
+                    <div style={{display:'flex'}}>
                             <label htmlFor="name">Task</label>
-                            <input type="text" placeholder='Task Name' name='name'/>
+                            <input type="text" 
+                                    placeholder='Task Name' 
+                                    name='name' 
+                                    value={name} 
+                                    onChange={(e) => 
+                                        setName(e.target.value)} 
+                                    ref={nameInputRef}
+                            />
                     </div>
-                    <div>
+                    <div style={{display:'flex'}}>
                             <label htmlFor="date">Task</label>
-                            <input type="date" name='date'/>
+                            <input type="date" 
+                                    name='date'
+                                    value={date} 
+                                    onChange={(e) => 
+                                        setDate(e.target.value)}
+                            />
                     </div>
-                    <button className='--btn --btn-success --btn-block --p'>Save Task</button>
+                    <button className='--btn --btn-success --btn-block --p'>
+                        {isEditing ? "Edit Task": 'Save Task'}
+                    </button>
                 </form>
             </div>
         </div>
@@ -26,7 +119,19 @@ const TaskManager = () => {
             <h2 className='--text-light'>Task List</h2>
             <hr style={{backgroundColor: '#fff'}}/>
             </div>
-            <Task />
+            {tasks.length === 0 ? (
+                <p className='--text-light'>No task added...</p>
+            ):(
+                <div>
+                    {tasks.map((task) => {
+                        return <Task 
+                                    {...task} 
+                                    editTask={editTask} 
+                                    deleteTask={deleteTask} 
+                                    completeTask={completeTask}/>;
+                    })}
+                </div>
+            )}
         </article>
     </div>
     
